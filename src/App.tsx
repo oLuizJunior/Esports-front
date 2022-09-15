00b360c0ac0a714458a0e060c653b1d7a83328ed
oLuizJunior <junior.luiz182@gmail.com>
2022-09-15 17:43:29 -0300

    modificações no app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { MagnifyingGlassPlus } from 'phosphor-react';
+
 import './styles/main.css'
 
 import logoImg from './assets/Logo.svg'
@@ -8,7 +9,7 @@ function App() {
   <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
     <img src={logoImg} alt="Logo Esportes" />
 
-    <h1 className=" text-6xl text-white font-black mt-20">
+    <h1 className="text-6xl text-white font-black mt-20">
       Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui
     </h1>
 
